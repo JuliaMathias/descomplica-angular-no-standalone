83906499a8e0320a5ae49db495ac3bff90b92a05
JuliaMathias <juliamss@gmail.com>
2024-04-26 19:09:10 -0300

    add subroutes to filhos in pai component

--- src/app/pages/pai.component.ts
+++ src/app/pages/pai.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   template: `
     <h1>Pai</h1>
     <p>Este é o pai</p>
+    <a [routerLink]="['filho1']"> Ir para componente filho1</a><br />
+    <a [routerLink]="['filho2']"> Ir para componente filho2</a><br />
+    <router-outlet></router-outlet>
   `,
 })
 export class PaiPageComponent {}

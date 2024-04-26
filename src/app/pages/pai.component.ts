ec66d055d40067fafedbab8b43f604af49d3f8f7
JuliaMathias <juliamss@gmail.com>
2024-04-26 19:07:34 -0300

    create pai component

---
+++ src/app/pages/pai.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pai',
+  template: `
+    <h1>Pai</h1>
+    <p>Este é o pai</p>
+  `,
+})
+export class PaiPageComponent {}

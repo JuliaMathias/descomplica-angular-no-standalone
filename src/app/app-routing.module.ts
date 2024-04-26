a57a191448b98318595094d5ca381b9c8326b2f1
JuliaMathias <juliamss@gmail.com>
2024-04-26 19:08:31 -0300

    add routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaiPageComponent } from './pages/pai.component';
+import { Filho1PageComponent } from './pages/filho1.component';
+import { Filho2PageComponent } from './pages/filho2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pai',
+    component: PaiPageComponent,
+    children: [
+      { path: 'filho1', component: Filho1PageComponent },
+      { path: 'filho2', component: Filho2PageComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

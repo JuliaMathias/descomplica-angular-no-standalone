d554137369a5fe47d4f3ca784a35cc637286ee86
JuliaMathias <juliamss@gmail.com>
2024-04-26 19:08:02 -0300

    create filho2 component

---
+++ src/app/pages/filho2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filho2',
+  template: `
+    <h1>Filho 2</h1>
+    <p>Este é o filho 2</p>
+  `,
+})
+export class Filho2PageComponent {}
